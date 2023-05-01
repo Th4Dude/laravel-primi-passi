3b2ac9d23f14f8ceb374b59cacbaad2d28b3ef17
Th4Dude <gallottimaurizio@yahoo.it>
2023-05-01 14:00:05 +0300

    Prima Pagina

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'title' => 'Prove con laravel',
+        'text' => 'Vediamo cosa succede',
+    ];
+    return view('home', $data);
 });
+
